d862f66f5e670d2a1f8b4f40fffc7e49755385c6
Zac Shaffer <shafferz@allegheny.edu>
2017-04-23 15:19:39 -0400

    Site v1.1.0

--- email_us.php
+++ email_us.php
@@ -1,8 +1,21 @@
+/*
+We found this code by looking for information on Stack Overflow at:
+http://stackoverflow.com/questions/712392/send-email-using-the-gmail-smtp-server-from-a-php-page
+*/
+
 <?php
-    $to = $_POST["email"];
-    $subject = "User Submitted Comment";
-    $message = $_POST["comment"];
-    $from = "address@someisp.com";
-    $headers = "From:" . $_POST["name"];
-    mail($to,$subject,$message,$headers);
+require_once 'swiftmailer/lib/swift_required.php';
+
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+  ->setUsername('cs220s2017.hlz')
+  ->setPassword('teamwork69');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+$message = Swift_Message::newInstance($_GET["email"])
+  ->setFrom(array('cs220s2017.hlz@gmail.com' => $_GET["name"]))
+  ->setTo(array('cs220s2017.hlz@gmail.com'))
+  ->setBody($_POST["comment"]);
+
+$result = $mailer->send($message);
 ?>
--- java/EmailForm.java
+++
@@ -1,8 +0,0 @@
-import java.applet.Applet;
-import java.awt.Graphics;
-
-public class EmailForm extends Applet {
-    public void paint(Graphics g) {
-        g.drawString("Hello world!", 50, 25);
-    }
-}
